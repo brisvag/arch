9a0f55d79c7026343df4b5233f24ae8f1afe70af
Lorenzo Gaifas <brisvag@gmail.com>
2019-08-15 18:19:58 +0200

    4th test

--- update.py
+++ update.py
@@ -3,10 +3,20 @@
 import subprocess
 from pathlib import Path
 
+
 repo_dir = Path(__file__).parent / 'repo'
 
-for dir in repo_dir.iterdir():
-    if dir.is_dir():
-        subprocess.run(['makepkg', '-fc'], cwd=dir)
 
-subprocess.run('repo-add brisvag.db.tar.xz ./*/*.tar.xz', cwd=repo_dir, shell=True)
+def build_packages():
+    for dir in repo_dir.iterdir():
+        if dir.is_dir():
+            subprocess.run(['makepkg', '-fc'], cwd=dir)
+
+
+def make_db():
+    subprocess.run('repo-add brisvag.db.tar.xz ./*/*.tar.xz', cwd=repo_dir, shell=True)
+
+
+if __name__ == '__main__':
+    build_packages()
+    make_db()
