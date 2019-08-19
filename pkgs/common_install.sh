36d3f19f47a7f775b7e5e4f5f622cb36cd356868
Lorenzo Gaifas <brisvag@gmail.com>
2019-08-19 20:28:26 +0200

    test

--- pkgs/common_install.sh
+++ pkgs/common_install.sh
@@ -1,29 +1,29 @@
-post_install() {
-  (
-  cd "${rootdir}"/dotfiles
-  files=($(find ${pkgdir} -type f))
-  (
-  cd ${HOME}
-  for file in ${files}; do
-    if [[ -f ${file} ]]; then
-      mv ${file} ${file}.pacbak
-    fi
-    ln -s "dotfiles/${file}" "${file}"
-  done
-  )
-  )
-}
-
 #post_install() {
 #  (
-#  echo "${rootdir}"/dotfiles
+#  cd "${rootdir}"/dotfiles
 #  files=($(find ${pkgdir} -type f))
-#  echo ${files}
 #  (
 #  cd ${HOME}
 #  for file in ${files}; do
-#    echo ${file}
+#    if [[ -f ${file} ]]; then
+#      mv ${file} ${file}.pacbak
+#    fi
+#    ln -s "dotfiles/${file}" "${file}"
 #  done
 #  )
 #  )
 #}
+
+post_install() {
+  (
+  echo "${rootdir}"/dotfiles
+  files=($(find ${pkgdir} -type f))
+  echo ${files}
+  (
+  cd ${HOME}
+  for file in ${files}; do
+    echo ${file}
+  done
+  )
+  )
+}
