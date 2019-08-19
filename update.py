86d8c2fd2cbb9bbb18a47f411eba5abf736a1a96
Lorenzo Gaifas <brisvag@gmail.com>
2019-08-19 14:26:55 +0200

    6th test

--- update.py
+++ update.py
@@ -4,6 +4,7 @@ import subprocess
 from pathlib import Path
 
 
+repo_name = 'brisvag'
 repo_dir = Path(__file__).parent / 'repo'
 
 
@@ -14,7 +15,14 @@ def build_packages():
 
 
 def make_db():
-    subprocess.run('repo-add brisvag.db.tar.xz ./*/*.tar.xz', cwd=repo_dir, shell=True)
+    subprocess.run(f'repo-add {repo_name}.db.tar.xz ./*/*.tar.xz', cwd=repo_dir, shell=True)
+    # replace symlinks with the real files
+    db_dest = repo_dir / f'{repo_name}.db'
+    files_dest = repo_dir / f'{repo_name}.files'
+    db_old = repo_dir / f'{repo_name}.db.tar.xz'
+    files_old = repo_dir / f'{repo_name}.files.tar.xz'
+    db_old.rename(db_dest)
+    files_old.rename(files_dest)
 
 
 if __name__ == '__main__':
