169cfbfdfa7cc0c03d09831b93b9d1e6d5b448a3
Lorenzo Gaifas <brisvag@gmail.com>
2019-08-20 11:07:43 +0200

    test

--- pkgs/common_install.sh
+++ pkgs/common_install.sh
@@ -1,13 +1,14 @@
 post_install() {
-  (
-  echo "${rootdir}"/dotfiles
-  files=($(find ${pkgdir} -type f))
-  echo ${files}
-  (
-  cd ${HOME}
-  for file in ${files}; do
-    echo ${file}
-  done
-  )
-  )
+#  (
+#  echo "${rootdir}"/dotfiles
+#  files=($(find ${pkgdir} -type f))
+#  echo ${files}
+#  (
+#  cd ${HOME}
+#  for file in ${files}; do
+#    echo ${file}
+#  done
+#  )
+#  )
+  echo this is version $1
 }
