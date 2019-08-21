538577b419d086989fe3db3d454121395ab1036f
Lorenzo Gaifas <brisvag@gmail.com>
2019-08-21 16:55:45 +0200

    changed handling of updates on db

--- update_db.py
+++ update_db.py
@@ -9,7 +9,7 @@ pkgs_dir = Path(__file__).parent / 'pkgs'
 repo_dir = Path(__file__).parent / 'repo'
 
 
-def build_db():
+def build_packages():
     for dir in pkgs_dir.iterdir():
         if dir.is_dir():
             subprocess.run(['makepkg', '-fc'], cwd=dir)
@@ -17,22 +17,29 @@ def build_db():
             subprocess.run(f'mv *.tar.xz ../../repo/', cwd=dir, shell=True)
 
 
-def fix_db():
+def update_db():
     for tar in repo_dir.iterdir():
         if tar.match(f'{repo_name}-*.pkg.tar.xz'):
             subprocess.run(f'repo-add {repo_name}.db.tar.xz {tar.name}', cwd=repo_dir, shell=True)
 
-    # replace symlinks with the real files. Hacky but necessary, cause symlinks somehow are not resolved
-    # db_dest = repo_dir / f'{repo_name}.db'
-    # files_dest = repo_dir / f'{repo_name}.files'
-    # db_old = repo_dir / f'{repo_name}.db.tar.xz'
-    # files_old = repo_dir / f'{repo_name}.files.tar.xz'
-    # db_old.rename(db_dest)
-    # files_old.rename(files_dest)
+
+def rename_db(backwards=False):
+    # move tar databases to correct names (without .tar.xz)
+    # hacky but necessary, because symlinks, somehow, are not resolved
+    db = repo_dir / f'{repo_name}.db'
+    files = repo_dir / f'{repo_name}.files'
+    db_tar = repo_dir / f'{repo_name}.db.tar.xz'
+    files_tar = repo_dir / f'{repo_name}.files.tar.xz'
+    if backwards:
+        db_tar, db = db, db_tar
+        files_tar, files = files, files_tar
+    if db_tar.is_file() and files_tar.is_file():
+        db_tar.rename(db)
+        files_tar.rename(files)
 
 
 if __name__ == '__main__':
-    # TODO: hacky. Should do a nicer update, instead of deleting everything
-    # subprocess.run(f'rm -rf ./repo/{repo_name}*', cwd=Path(__file__).parent, shell=True)
-    build_db()
-    fix_db()
+    rename_db(backwards=True)
+    build_packages()
+    update_db()
+    rename_db()
