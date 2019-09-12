6e6cb9e7ba7ad3b5adb77867cbfa4934b952b90c
Lorenzo Gaifas <brisvag@gmail.com>
2019-09-12 15:14:10 +0200

    changed backup system

--- pkgs/scripts/dotfiles/scripts/backup.sh
+++ pkgs/scripts/dotfiles/scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -Partx --delete --stats --exclude 'lost+found/' --exclude 'mnt/' --exclude 'tmp/' --exclude 'var/tmp/' --exclude 'var/cache/' --exclude 'home/brisvag/.local/share/Steam/steamapps/common/Age2HD' --exclude '/home/brisvag/.cache' / /mnt/data/backup
+rsync -Partx --delete --stats --exclude '.local/share/Steam/steamapps/common/Age2HD' --exclude '.cache' /home/brisvag /mnt/backup
