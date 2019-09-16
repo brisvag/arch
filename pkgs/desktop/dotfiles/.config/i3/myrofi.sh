f98020a874ce89076b5b81f966aa0c4144b85489
Lorenzo Gaifas <brisvag@gmail.com>
2019-09-16 14:05:04 +0200

    added comment

--- pkgs/desktop/dotfiles/.config/i3/myrofi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-# rofi as a dmenu alternative
-
-rofi -show drun -lines 5 -eh 2 -width 100 -padding 800 -font "inconsolata 14" -opacity -bw 0 -hide-scrollbar -color-normal "#001F1F1F,#929292,#001F1F1F,#00414141,#FFFFFF" -color-active "#001F1F1F,#929292,#001F1F1F,#00414141,#FFFFFF" -color-urgent "#001F1F1F,#929292,#001F1F1F,#00414141,#FFFFFF" -color-window "#d31F1F1F,#d31F1F1F,#d31F1F1F"
